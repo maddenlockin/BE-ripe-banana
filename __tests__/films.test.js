5489474398f642db034cd0d9eaa2025e492f70af
maddenlockin <maddenlockin@gmail.com>
2021-10-11 16:34:45 -0700

    remove app.test

---
+++ __tests__/films.test.js
@@ -0,0 +1,32 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+describe('r-b-h routes', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    const film = {
+        title: 'Appendectomy Unlimited',
+        studioId: 4,
+        released: 1963,
+    };
+
+    it('posts new film to db', () => {
+        return request(app)
+            .post('/api/films')
+            .send(film)
+            .then((res) => {
+                expect(res.body).toEqual({
+                    filmId: '4',
+                    ...film,
+                });
+            });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
+});
---
+++ __tests__/reviews.test.js
@@ -0,0 +1,32 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+describe('r-b-h routes', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    const studio = {
+        studioName: 'Ripe Banana Hell',
+        city: 'Portland',
+        state: 'OR',
+        country: 'USA'
+    };
+
+    xit('posts new studio to db', () => {
+        return request(app)
+            .post('/api/studios')
+            .send(studio)
+            .then((res) => { expect(res.body).toEqual({
+                studioId: '1',
+                ...studio,
+            });
+            });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
+});
